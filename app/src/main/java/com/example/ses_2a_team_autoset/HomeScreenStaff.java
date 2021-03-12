6b5b589b89533c2a049b3d1b55950f4e4743f5e5
Vladimir Poshevelya <vposhvv@yandex.ru>
2021-03-12 15:26:36 +1100

    Login function ready

---
+++ app/src/main/java/com/example/ses_2a_team_autoset/HomeScreenStaff.java
@@ -0,0 +1,18 @@
+package com.example.ses_2a_team_autoset;
+
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreenStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen_staff);
+    }
+
+}
---
+++ app/src/main/java/com/example/ses_2a_team_autoset/Login.java
@@ -0,0 +1,95 @@
+package com.example.ses_2a_team_autoset;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
+import android.content.Intent;
+import android.text.TextUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+
+public class Login extends AppCompatActivity {
+    EditText etUsername,etPassword;
+    Button btSubmit;
+    //Firebase
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance("https://ses-2a-studybuddies-default-rtdb.firebaseio.com/");
+        users = database.getReference("Users");
+
+        etUsername = findViewById(R.id.et_userID);
+        etPassword = findViewById(R.id.et_password);
+        btSubmit = findViewById(R.id.bt_login);
+
+
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn(etUsername.getText().toString(),
+                        etPassword.getText().toString());
+
+            }
+        });
+
+
+    }
+
+    private void logIn(final String UID, final String UPassword) {
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(UID).exists()){
+                    if(!UID.isEmpty()){
+                        String password = dataSnapshot.child(UID).child("Password").getValue().toString();
+                        if(password.equals(UPassword)){
+                            String type = dataSnapshot.child(UID).child("Type").getValue().toString();
+                            if(type.equals("Student")){
+                                Toast.makeText(Login.this, "Success Login", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Login.this, HomeScreenStaff.class));
+                            }else if(type.equals("Staff")){
+                                Toast.makeText(Login.this, "Success Login", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Login.this, HomeScreenStudent.class));
+                            }else
+                                Toast.makeText(Login.this, "Wrong type", Toast.LENGTH_SHORT).show();
+
+                        }else
+                            Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else
+                    Toast.makeText(Login.this, "Wrong ID", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //custom code
+            }
+        });
+
+    }
+
+}
