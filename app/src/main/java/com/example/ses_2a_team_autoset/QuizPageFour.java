7c2f20363155bef3fb45bb8edb50d02512842226
helenatran <helena.e.tran@student.uts.edu.au>
2021-04-07 17:34:10 +1000

    Create quiz page 4 - Scenario Questions

--- app/src/main/java/com/example/ses_2a_team_autoset/QuizPageFour.java
+++ app/src/main/java/com/example/ses_2a_team_autoset/QuizPageFour.java
@@ -8,28 +8,28 @@ import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class QuizPageThree extends AppCompatActivity {
+public class QuizPageFour extends AppCompatActivity {
     Button btnLogout, btnNext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.quiz_page_three);
+        setContentView(R.layout.quiz_page_four);
 
-        btnLogout = findViewById(R.id.btn_logout_quizThree);
-        btnNext = findViewById(R.id.btn_next_quizThree);
+        btnLogout = findViewById(R.id.btn_logout_quizFour);
+        btnNext = findViewById(R.id.btn_next_quizFour);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(QuizPageThree.this, Login.class));
+                startActivity(new Intent(QuizPageFour.this, Login.class));
             }
         });
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(QuizPageThree.this, QuizPageFour.class));
+                startActivity(new Intent(QuizPageFour.this, HomeScreenStudent.class));
             }
         });
     }
