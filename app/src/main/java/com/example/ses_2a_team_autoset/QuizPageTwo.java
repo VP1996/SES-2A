1f85fd130e92c25c4c3ba397f6ca88fa56dab46d
helenatran <helena.e.tran@student.uts.edu.au>
2021-04-20 10:00:21 +1000

    Add select classes options [in progress]

--- app/src/main/java/com/example/ses_2a_team_autoset/QuizPageTwo.java
+++ app/src/main/java/com/example/ses_2a_team_autoset/QuizPageTwo.java
@@ -1,17 +1,22 @@
 package com.example.ses_2a_team_autoset;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Constraints;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -24,6 +29,7 @@ public class QuizPageTwo extends AppCompatActivity {
     TextView tvSubjects;
     boolean[] selectedSubjects;
     TextInputLayout tilFaculties;
+    LinearLayout mLayout;
     AutoCompleteTextView actvFaculties;
 
     ArrayList<Integer> subjectsList = new ArrayList<>();
@@ -58,6 +64,7 @@ public class QuizPageTwo extends AppCompatActivity {
         selectedSubjects = new boolean[subjectsArray.length];
         tilFaculties = findViewById(R.id.menu_faculties);
         actvFaculties = findViewById(R.id.actv_faculties);
+        mLayout = findViewById(R.id.llSelectClasses);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,6 +111,11 @@ public class QuizPageTwo extends AppCompatActivity {
                             }
                         }
                         tvSubjects.setText(stringBuilder.toString());
+
+                        //TODO: Create TextView dialogue
+                        for (int i = 0; i < subjectsList.size(); i++) {
+                            mLayout.addView(createNewTextViewSelectClasses(subjectsArray[subjectsList.get(i)]));
+                        }
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -131,5 +143,23 @@ public class QuizPageTwo extends AppCompatActivity {
         facultiesAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.menu_item, facultiesList);
         actvFaculties.setAdapter(facultiesAdapter);
         actvFaculties.setThreshold(1);
+    }
+
+    private TextView createNewTextViewSelectClasses(String text) {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        //TODO: Convert the margin to dp (from pixel to dp)
+        lparams.bottomMargin = 16;
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setBackgroundResource(R.drawable.select_box_background);
+        textView.setBackgroundColor(Color.parseColor("#E0E0E0"));
+        textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.select_box_arrow_down, 0);
+        textView.setCompoundDrawablePadding(16);
+        textView.setPadding(23, 23, 23, 23);
+        textView.setTextSize(16);
+        textView.setText(text);
+        return textView;
     }
 }
