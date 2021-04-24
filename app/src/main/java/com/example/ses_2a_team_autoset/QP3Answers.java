0271698981ae69d6595dbb1a039a7e92925f6b1f
helenatran <helena.e.tran@student.uts.edu.au>
2021-04-24 16:44:19 +1000

    Store quiz page 3 answers in database + validation

---
+++ app/src/main/java/com/example/ses_2a_team_autoset/QP3Answers.java
@@ -0,0 +1,103 @@
+package com.example.ses_2a_team_autoset;
+
+public class QP3Answers {
+    private String mcq1;
+    private String friendId;
+    private String mcq3;
+    private String mcq4;
+    private String mcq5;
+    private String mcq6;
+    private String mcq7;
+    private String mcq8;
+    private String mcq9;
+    private String mcq10;
+    private String mcq11;
+
+    public String getMcq1() {
+        return mcq1;
+    }
+
+    public void setMcq1(String mcq1) {
+        this.mcq1 = mcq1;
+    }
+
+    public String getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(String friendId) {
+        this.friendId = friendId;
+    }
+
+    public String getMcq3() {
+        return mcq3;
+    }
+
+    public void setMcq3(String mcq3) {
+        this.mcq3 = mcq3;
+    }
+
+    public String getMcq4() {
+        return mcq4;
+    }
+
+    public void setMcq4(String mcq4) {
+        this.mcq4 = mcq4;
+    }
+
+    public String getMcq5() {
+        return mcq5;
+    }
+
+    public void setMcq5(String mcq5) {
+        this.mcq5 = mcq5;
+    }
+
+    public String getMcq6() {
+        return mcq6;
+    }
+
+    public void setMcq6(String mcq6) {
+        this.mcq6 = mcq6;
+    }
+
+    public String getMcq7() {
+        return mcq7;
+    }
+
+    public void setMcq7(String mcq7) {
+        this.mcq7 = mcq7;
+    }
+
+    public String getMcq8() {
+        return mcq8;
+    }
+
+    public void setMcq8(String mcq8) {
+        this.mcq8 = mcq8;
+    }
+
+    public String getMcq9() {
+        return mcq9;
+    }
+
+    public void setMcq9(String mcq9) {
+        this.mcq9 = mcq9;
+    }
+
+    public String getMcq10() {
+        return mcq10;
+    }
+
+    public void setMcq10(String mcq10) {
+        this.mcq10 = mcq10;
+    }
+
+    public String getMcq11() {
+        return mcq11;
+    }
+
+    public void setMcq11(String mcq11) {
+        this.mcq11 = mcq11;
+    }
+}
