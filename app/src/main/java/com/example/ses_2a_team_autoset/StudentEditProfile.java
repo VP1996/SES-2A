49cda3b715cff9d3365f98862d95070216a08f96
helenatran <helena.e.tran@student.uts.edu.au>
2021-05-03 23:07:39 +1000

    Clean up code

--- app/src/main/java/com/example/ses_2a_team_autoset/StudentEditProfile.java
+++ app/src/main/java/com/example/ses_2a_team_autoset/StudentEditProfile.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.text.InputType;
 import android.view.ContextThemeWrapper;
 import android.view.View;
