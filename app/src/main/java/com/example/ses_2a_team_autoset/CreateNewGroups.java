eacbb8b7c6efc2ae2a8bc516e2af68c42baba797
Vladimir Poshevelya <vposhvv@yandex.ru>
2021-05-03 12:01:41 +1000

    Generate new groups for admin is working, need to allow select multiple tutorials.

--- app/src/main/java/com/example/ses_2a_team_autoset/CreateNewGroups.java
+++ app/src/main/java/com/example/ses_2a_team_autoset/CreateNewGroups.java
@@ -1,94 +1,242 @@
 package com.example.ses_2a_team_autoset;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.text.InputType;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CreateNewGroups extends AppCompatActivity {
 
-    EditText etFName,etLName,etStudentID,etEmail,etPassword;
-    Button btRegister,btGoBack;
-    Person newPerson;
-
-    //Firebase
+    Button btnGoBack, btnSubmit;
+    TextView tvSubjects;
+    boolean[] selectedSubjects;
+    TextInputLayout tGroupSize,tNumberOfGroups;
+    LinearLayout mLayout;
+    CurrentUser user;
     FirebaseDatabase database;
-    DatabaseReference users;
+    DatabaseReference users,reff1;
+    ArrayList<Integer> subjectsIndexList = new ArrayList<>();
+    ArrayList<String> subjectsList = new ArrayList<>();
+    ArrayList<String> selectedSubjectsList = new ArrayList<>();
+
+    String[] subjectsArray;
+
+
+    String[] classesArray = {"Tut1", "Tut2", "Tut3"};
+    ArrayList<String> classesList = new ArrayList<String>(Arrays.asList(classesArray));
+    ArrayList<TextInputLayout> classesLayoutList = new ArrayList<>();
+    ArrayAdapter<String> actvAdapter;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.newregister);
+        setContentView(R.layout.createnewgroups);
 
-        database = FirebaseDatabase.getInstance("https://ses-2a-studybuddies-default-rtdb.firebaseio.com/");
-        users = database.getReference("Users");
+        reff1 = FirebaseDatabase.getInstance().getReference().child("Subjects");
+        btnGoBack = findViewById(R.id.btn_goBack);
+        btnSubmit = findViewById(R.id.btn_submit);
+        tvSubjects = findViewById(R.id.tv_subjects);
+        tGroupSize = findViewById(R.id.GRoupSizes);//NumberOfGroups
+        tNumberOfGroups = findViewById(R.id.NumberOfGroups);
 
-        etFName = findViewById(R.id.et_FirstName);
-        etLName = findViewById(R.id.et_LastName);
-        etStudentID = findViewById(R.id.et_StudentID);
-        etEmail = findViewById(R.id.et_email);
-        etPassword = findViewById(R.id.et_password);
-        btRegister = findViewById(R.id.signup_btn);
-        btGoBack = findViewById(R.id.goback_btn);
+        mLayout = findViewById(R.id.llSelectClasses);
 
-        btGoBack.setOnClickListener(new View.OnClickListener() {
+        reff1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    subjectsList.add(dataSnapshot.getKey().toString());
+                }
+                selectedSubjects = new boolean[subjectsList.size()];
+                subjectsArray = subjectsList.toArray(new String[subjectsList.size()]);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(CreateNewGroups.this, "No subjects found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(CreateNewGroups.this, HomeScreenStaff.class));
             }
         });
 
-        btRegister.setOnClickListener(new View.OnClickListener() {
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!validateSubjects() | !validateTextInput(tNumberOfGroups)| !validateTextInput(tGroupSize) )
+                    return;
 
-                if(TextUtils.isEmpty(etFName.getText().toString().trim())) {
-                    etFName.setError("First Name is Required.");
-                    return;
-                }else if(TextUtils.isEmpty(etLName.getText().toString().trim())) {
-                    etLName.setError("Last Name is Required.");
-                    return;
-                }else if(TextUtils.isEmpty(etStudentID.getText().toString().trim())) {
-                    etStudentID.setError("Student ID is Required.");
-                    return;
-                }else if(TextUtils.isEmpty(etEmail.getText().toString().trim())) {
-                    etEmail.setError("Email is Required.");
-                    return;
-                }else if(TextUtils.isEmpty(etPassword.getText().toString().trim())) {
-                    etPassword.setError("Password is Required.");
-                    return;
-                }else {
-                    RegisterNewStudent(etFName.getText().toString(),
-                            etLName.getText().toString(),
-                            etStudentID.getText().toString(),
-                            etEmail.getText().toString(),
-                            etPassword.getText().toString());
-                }
+                saveDataG(selectedSubjectsList,
+                        tGroupSize.getEditText().getText().toString(),
+                        tNumberOfGroups.getEditText().getText().toString());
             }
         });
+
+        tvSubjects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialise alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(CreateNewGroups.this);
+                builder.setTitle("Select Subjects");
+                builder.setMultiChoiceItems(subjectsArray, selectedSubjects, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+                        if (isChecked) {
+                            if (!subjectsIndexList.contains(position))
+                                subjectsIndexList.add(position);
+                        } else {
+                            subjectsIndexList.remove((Integer.valueOf(position)));
+                            selectedSubjectsList.remove((Integer.valueOf(position)));
+                        }
+                    }
+                });
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mLayout.removeAllViews();
+                        classesLayoutList.clear();
+                        selectedSubjectsList.clear();
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (int i = 0; i < subjectsIndexList.size(); i++) {
+                            selectedSubjectsList.add(subjectsArray[subjectsIndexList.get(i)]);
+                            //Concat array value
+                            stringBuilder.append(subjectsArray[subjectsIndexList.get(i)]);
+                            if (i != subjectsIndexList.size() - 1) {
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        tvSubjects.setText(stringBuilder.toString());
+
+                        for (int i = 0; i < subjectsIndexList.size(); i++) {
+                            TextInputLayout tilClass = createNewTextViewSelectClasses(subjectsArray[subjectsIndexList.get(i)]);
+                            classesLayoutList.add(tilClass);
+                            mLayout.addView(tilClass);
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mLayout.removeAllViews();
+                        classesLayoutList.clear();
+                        for (int i = 0; i < selectedSubjects.length; i++) {
+                            //Remove all selections
+                            selectedSubjects[i] = false;
+                            subjectsIndexList.clear();
+                            tvSubjects.setText("");
+                        }
+                    }
+                });
+                //Show dialog
+                builder.show();
+            }
+        });
+
     }
 
-    private void RegisterNewStudent(String FName, String LastName, String StudentID, String Email, String Password) {
-        newPerson = new Person();
-        newPerson.setFirstName(FName);
-        newPerson.setLastName(LastName);
-        newPerson.setEmail(Email);
-        newPerson.setPassword(Password);
-        newPerson.setType("Student");
-        newPerson.setQuizTaken("0");
+    private TextInputLayout createNewTextViewSelectClasses(String text) {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lparams.bottomMargin = 20;
+        TextInputLayout textInputLayout = new TextInputLayout(new ContextThemeWrapper(this, R.style.Widget_MaterialComponents_TextInputLayout_FilledBox_ExposedDropdownMenu));
+        textInputLayout.setBoxBackgroundMode(TextInputLayout.BOX_BACKGROUND_FILLED);
+        textInputLayout.setLayoutParams(lparams);
+        AutoCompleteTextView actv = new AutoCompleteTextView(textInputLayout.getContext());
+        actv.setHint(text);
+        actv.setPadding(40, 60, 40, 60);
+        actv.setInputType(InputType.TYPE_NULL);
+        actv.setTextSize(16);
 
-        users.child(StudentID).setValue(newPerson);
-        Toast.makeText(CreateNewGroups.this, "New Student Added", Toast.LENGTH_SHORT).show();
+        actvAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.menu_item, classesList);
+        actv.setAdapter(actvAdapter);
+        actv.setThreshold(1);
+
+        textInputLayout.addView(actv, lparams);
+        return textInputLayout;
+    }
+
+    private void saveDataG(ArrayList<String> subjects, String GroupSizes,String NumberOfGroups) {
+
+        int sGroupSizes = Integer. parseInt(GroupSizes);
+        int sNumberOfGroups = Integer. parseInt(NumberOfGroups);
+
+        for (int i = 0; i < subjects.size(); i ++) {
+            String classString = classesLayoutList.get(i).getEditText().getText().toString();
+            String subjectString = selectedSubjectsList.get(i);
+            reff1.child(subjectString).child(classString).child("GroupSizes").setValue(GroupSizes);
+
+
+            for(int f = 1; f <= sNumberOfGroups;f++){
+
+                for(int x = 1; x <= sGroupSizes;x++){
+                    String gn = "Group"+String.valueOf(f);
+                    reff1.child(subjectString).child(classString).child("Groups").child(gn).child(String.valueOf(x)).setValue("1");
+
+                }
+            }
+
+        }
+        Toast.makeText(CreateNewGroups.this, "New Groups Added", Toast.LENGTH_SHORT).show();
         reload();
+    }
 
+    private boolean validateTextInput(TextInputLayout textInput) {
+        String input = textInput.getEditText().getText().toString().trim();
+        String inputName = (String) textInput.getHint();
+        if (input.isEmpty()) {
+            textInput.setError(inputName + " is required");
+            return false;
+        } else {
+            textInput.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateSubjects() {
+        if (selectedSubjectsList.isEmpty()) {
+            tvSubjects.setError("Subjects are required");
+            return false;
+        } else {
+            tvSubjects.setError(null);
+            return true;
+        }
     }
     public void reload() {
         Intent intent = getIntent();
